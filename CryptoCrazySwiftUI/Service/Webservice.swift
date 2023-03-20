015ba127483cd48e41dfbcba0aa78c045eb5c33a
serkanboyraci <serkanboyraci@gmail.com>
2023-03-20 22:55:59 +0300

    webService added

---
+++ CryptoCrazySwiftUI/Service/Webservice.swift
@@ -0,0 +1,39 @@
+//
+//  Webservice.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Ali serkan Boyracı  on 20.03.2023.
+//
+
+import Foundation
+
+class Webservice {
+    func downloadCurrencies(url: URL,
+                            completion: @escaping(Result<[CryptoCurrency], DownloaderError>)-> Void) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.failure(.badUrl))
+            }
+            
+            guard let data = data, error == nil else {
+                return completion(.failure(.noData))
+            }
+            
+            guard let currencies = try? JSONDecoder().decode([CryptoCurrency].self, from: data) else {
+                return completion(.failure(.dataParseError))
+            }
+            
+            completion(.success(currencies))
+            
+        }.resume()
+    }
+}
+
+enum DownloaderError : Error {
+    case badUrl
+    case noData
+    case dataParseError
+}
+    
