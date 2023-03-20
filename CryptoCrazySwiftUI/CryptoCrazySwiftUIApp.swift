49181772f139f1146637b10e62f30b663c631235
serkanboyraci <serkanboyraci@gmail.com>
2023-03-20 19:17:19 +0300

    Initial Commit

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Ali serkan Boyracı  on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
